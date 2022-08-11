2d305523ec784fc2796ec3f9f292c30ec51f4104
huynq <huynqhe140638@fpt.edu.vn>
2022-08-11 09:46:34 +0700

    refactor: make analyze

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,14 +1,29 @@
-import 'package:flutter/material.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:revup_provider/login/bloc/login_bloc.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders Login', (tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: Scaffold(),
-        ),
-      );
+  group('LoginBloc', () {
+    late LoginBloc loginBloc;
+
+    setUp(() {
+      loginBloc = LoginBloc();
     });
+    blocTest<LoginBloc, LoginState>(
+      'emit Login Ready state when Login Start event is added',
+      build: () => loginBloc,
+      act: (bloc) =>
+          bloc.add(const LoginEvent.start(isLoginButtonEnabled: false)),
+      expect: () =>
+          <LoginState>[const LoginState.ready(isLoginButtonEnabled: false)],
+    );
+    blocTest<LoginBloc, LoginState>(
+      'emit sucess state when Submit event is added',
+      build: () => loginBloc,
+      act: (bloc) =>
+          bloc.add(const LoginEvent.submit(phoneNumber: '123456789')),
+      expect: () =>
+          <LoginState>[const LoginState.success(phoneNumber: '123456789')],
+    );
   });
 }
