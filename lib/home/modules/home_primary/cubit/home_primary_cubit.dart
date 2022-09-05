c621f6ffeb8d38a742ffab6411a755aff7283ddf
EbonyMaw <48524817+wamynobe@users.noreply.github.com>
2022-09-05 08:45:02 +0700

    Hotfix/realtime homeprimary page (#57)
    * hotfix: change primay page to real time upadte

--- lib/home/modules/home_primary/cubit/home_primary_cubit.dart
+++ lib/home/modules/home_primary/cubit/home_primary_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -29,6 +31,7 @@ class HomePrimaryCubit extends Cubit<HomePrimaryState> {
   final IStore<AppUser> userRepos;
   final IStore<RepairRecord> recordRepos;
   final StoreRepository storeRepos;
+  StreamSubscription<QuerySnapshot<Map<String, dynamic>>>? _s;
   final imageList = IVector.from([
     'https://www.tiendauroi.com/wp-content/uploads/2020/02/shopee-freeship-xtra-750x233.jpg',
     'https://e-magazine.asiamedia.vn/wp-content/uploads/2021/07/top-10-hang-dau-nhot-noi-tieng-nhat-tai-viet-nam-21.jpg',
@@ -36,75 +39,78 @@ class HomePrimaryCubit extends Cubit<HomePrimaryState> {
     'https://e-magazine.asiamedia.vn/wp-content/uploads/2021/07/top-10-hang-dau-nhot-noi-tieng-nhat-tai-viet-nam-21.jpg',
   ]);
   Future<Unit> onStarted() async {
-    final user =
-        (await userRepos.get(userId)).getOrElse(() => throw NullThrownError());
-    final listRecentOrder = (await recordRepos.where('pid', isEqualTo: userId))
-        .map((r) => r)
-        .fold<IList<RepairRecord>>(
-          (l) => nil<RepairRecord>(),
-          (r) => r.sort(
-            orderBy(
-              StringOrder.reverse(),
-              (a) => a.created.toString(),
-            ),
-          ),
-        );
-    final listSv = (await storeRepos.repairCategoryRepo(user).all()).map(
-      (r) => r.map(
-        (a) async => (await storeRepos.repairServiceRepo(user, a).all())
-            .fold((l) => nil<RepairService>(), (r) => r),
-      ),
-    );
-    final l = await listSv.fold<Future<List<IList<RepairService>>>>(
-      (l) async => Future.wait(nil<Future<IList<RepairService>>>().toList()),
-      (r) async => Future.wait(
-        r.toList(),
-      ),
-    );
-    final data = l
-        .fold<IList<RepairService>>(
-          l.toList()[0],
-          (previousValue, element) => previousValue.plus(element),
-        )
-        .map(
-          (sv) => MyServiceData(
-            fee: sv.fee,
-            imgUrl: sv.img ?? '',
-            name: sv.name,
-            isActive: sv.active,
-          ),
-        );
-    final list = listRecentOrder.toList();
-    if (list.isEmpty) {
-      emit(
-        HomePrimaryState.recentOrderEmpty(
-          user: user,
-          ads: imageList,
-          listService: data.toList(),
-        ),
-      );
-    } else {
-      final consumer = (await userRepos.get(list[0].cid))
+    _s = userRepos.collection().snapshots().listen((event) async {
+      final user = (await userRepos.get(userId))
           .getOrElse(() => throw NullThrownError());
-      final rc = RecentOrderData(
-        created: list[0].created,
-        imageUrl: consumer.avatarUrl,
-        providerName: '${consumer.firstName} ${consumer.lastName}',
-        rating: list[0].maybeMap(
-          orElse: () => 0,
-          finished: (value) => value.feedback.rating,
+      final listRecentOrder =
+          (await recordRepos.where('pid', isEqualTo: userId))
+              .map((r) => r)
+              .fold<IList<RepairRecord>>(
+                (l) => nil<RepairRecord>(),
+                (r) => r.sort(
+                  orderBy(
+                    StringOrder.reverse(),
+                    (a) => a.created.toString(),
+                  ),
+                ),
+              );
+      final listSv = (await storeRepos.repairCategoryRepo(user).all()).map(
+        (r) => r.map(
+          (a) async => (await storeRepos.repairServiceRepo(user, a).all())
+              .fold((l) => nil<RepairService>(), (r) => r),
         ),
-        serviceType: list[0].vehicle == 'motorbike' ? 0 : 1,
       );
-      emit(
-        HomePrimaryState.loaded(
-          user: user,
-          recentOrder: rc,
-          listService: data.toList(),
-          ads: imageList,
+      final l = await listSv.fold<Future<List<IList<RepairService>>>>(
+        (l) async => Future.wait(nil<Future<IList<RepairService>>>().toList()),
+        (r) async => Future.wait(
+          r.toList(),
         ),
       );
-    }
+      final data = l
+          .fold<IList<RepairService>>(
+            l.toList()[0],
+            (previousValue, element) => previousValue.plus(element),
+          )
+          .map(
+            (sv) => MyServiceData(
+              fee: sv.fee,
+              imgUrl: sv.img ?? '',
+              name: sv.name,
+              isActive: sv.active,
+            ),
+          );
+      final list = listRecentOrder.toList();
+      if (list.isEmpty) {
+        emit(
+          HomePrimaryState.recentOrderEmpty(
+            user: user,
+            ads: imageList,
+            listService: data.toList(),
+          ),
+        );
+      } else {
+        final consumer = (await userRepos.get(list[0].cid))
+            .getOrElse(() => throw NullThrownError());
+        final rc = RecentOrderData(
+          created: list[0].created,
+          imageUrl: consumer.avatarUrl,
+          providerName: '${consumer.firstName} ${consumer.lastName}',
+          rating: list[0].maybeMap(
+            orElse: () => 0,
+            finished: (value) => value.feedback.rating,
+          ),
+          serviceType: list[0].vehicle == 'motorbike' ? 0 : 1,
+        );
+        emit(
+          HomePrimaryState.loaded(
+            user: user,
+            recentOrder: rc,
+            listService: data.toList(),
+            ads: imageList,
+          ),
+        );
+      }
+    });
 
     return unit;
   }
