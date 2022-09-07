c4e5ef497300ec11abaa4e0a253846fec982240c
huynq <huynqhe140638@fpt.edu.vn>
2022-09-08 00:14:04 +0700

    fix: avatar default img

--- lib/vendor_authentication/view/update_profile_view.dart
+++ lib/vendor_authentication/view/update_profile_view.dart
@@ -75,7 +75,8 @@ class UpdateProfileView extends StatelessWidget {
                       Avatar(
                         localImg: File(''),
                         urlImage: user.avatarUrl,
-                        userName: 'Revup',
+                        userName:
+                            user.lastName.isEmpty ? 'Revup' : user.lastName,
                         callback: () {
                           _showModalButtonSheet(context, 0, list);
                         },
@@ -101,7 +102,8 @@ class UpdateProfileView extends StatelessWidget {
                         Avatar(
                           localImg: file[0],
                           urlImage: user.avatarUrl,
-                          userName: 'Revup',
+                          userName:
+                              user.lastName.isEmpty ? 'Revup' : user.lastName,
                           callback: () {
                             _showModalButtonSheet(context, 0, list);
                           },
